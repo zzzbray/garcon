7355fca846f068f18285f2ef64872da7d3876577
shehanpeiris <shehanpeiris@gmail.com>
2019-05-08 21:05:53 -0500

    included correct App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 // Pages
 import LandingPage from "./pages/LandingPage"
@@ -12,33 +12,58 @@ import Waitstaff2OrderPage from "./pages/Waitstaff2OrderPage";
 import Waitstaff3ReceiptPage from "./pages/Waitstaff3ReceiptPage";
 import './App.css';
 
+class App extends Component {
+
+  // Testing State
+  state = {
+    menu: []
+  }
+
+  componentDidMount() {
+    this.getMenu();
+  }
+
+  getMenu = _ => {
+    fetch("http://localhost:3006/api/menu")
+      .then(response => response.json())
+      // .then(({data}) => {
+      //   console.log(data)
+      // })
+      .then(response => this.setState({ menu: response }))
+      .then(console.log(this.state.menu))
+  };
+
+  renderMenu = ({menu_id, menu_name}) => <div key={menu_id}>{menu_name}</div>;
+
 // Components
+// function App() {
+  render() {
+    const { menu } = this.state;
 
+    return (
+      <div className="App">
+        {this.state.menu.map(this.renderMenu)}        
+        {/* <div> */}
+          {/* Routing will go here */}
+          {/* <LandingPage /> */}
+          {/* <Login /> */}
+          {/* <Guest1SignUpLogin /> */}
+          {/* <Guest2ResMake /> */}
+          {/* <Guest3ResConf /> */}
+          {/* <ManagerPage /> */}
+          {/* <Waitstaff1CurTablesPage /> */}
+          <Waitstaff2OrderPage />
+          {/* <Waitstaff3ReceiptPage /> */}
+        {/* </div> */}
 
-
-function App() {
-  return (
-    <div className="App">
-      <div>
-        {/* Routing will go here */}
-        {/* <LandingPage /> */}
-        {/* <Login /> */}
-        {/* <Guest1SignUpLogin /> */}
-        {/* <Guest2ResMake /> */}
-        {/* <Guest3ResConf /> */}
-        {/* <ManagerPage /> */}
-        {/* <Waitstaff1CurTablesPage /> */}
-        <Waitstaff2OrderPage />
-        {/* <Waitstaff3ReceiptPage /> */}
       </div>
-
-    </div>
-  );
-}
+    );
+  }
+// }
+};
 
 export default App;
 
 
 // Add Router
 // Add all components
-
