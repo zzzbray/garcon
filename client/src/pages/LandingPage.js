82afcb55ce33a6400f6757932a28327f94c29493
shehanpeiris <shehanpeiris@gmail.com>
2019-05-08 21:02:28 -0500

    express structure with Zach's front end changes

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import "./login.css";
+import "../landingpage.css";
 
-// This is Ryan's index.html
-// Logic is located at index.js in the src folder
+// This is Ryan's slick code
+// Logic is located below export
 
 function LandingPage(){
   return (
@@ -11,11 +11,11 @@ function LandingPage(){
       Landing Page
     </h1>
     <div className="split left">
-      {/* <h1>Cochon Volant Brasserie</h1> */}
+      <h1>Cochon Volant Brasserie</h1>
       <a href="login.html" className="button"> Piggy</a>
     </div>
     <div className="split right">
-      {/* <h1>Taureaux Tavern</h1> */}
+      <h1>Taureaux Tavern</h1>
       <a href="login.html" className="button"> Bully</a>
     </div>
   </div>
@@ -24,4 +24,26 @@ function LandingPage(){
 
 export default LandingPage;
 
-// conditional rendering
+// conditional rendering - this will make the js work - Matt
+
+// This doesn't work right now
+
+// const left = document.querySelector('.left');
+// const right = document.querySelector('.right');
+// const container = document.querySelector('.container');
+
+// left.addEventListener('mouseenter', () => {
+//     container.classList.add('hover-left');
+// });
+
+// left.addEventListener('mouseleave', () => {
+//     container.classList.remove('hover-left');
+// });
+
+// right.addEventListener('mouseenter', () => {
+//     container.classList.add('hover-right');
+// });
+
+// right.addEventListener('mouseleave', () => {
+//     container.classList.remove('hover-right');
+// });
