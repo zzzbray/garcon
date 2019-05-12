78d64b4fd13aaa70013cd4e4b7657c5735d88c3f
zzzbray <zachdbray@gmail.com>
2019-05-12 16:37:08 -0500

    removed Staff and Menu items from WaitstaffNavBar as only navigation they will need will be to get to the current tables table

--- client/src/pages/Waitstaff1CurTablesPage.js
+++ client/src/pages/Waitstaff1CurTablesPage.js
@@ -2,11 +2,24 @@ import React from 'react';
 import WaitstaffNavBar from "../components/Waitstaff/WaitstaffNavBar.js"
 import CurrentTablesComp from "../components/Waitstaff/CurrentTablesComp.js"
 import Nav from "react-bootstrap/Nav"
+import "./manager.css"
+
+
+
 function Waitstaff1CurTablesPage(){
   return (
   <div>
     
     <WaitstaffNavBar />
+    <header id="header">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10">
+          <h2>Cochon Volant Brasserie</h2>
+        </div>
+      </div>
+    </div>
+  </header>
     <CurrentTablesComp />
 
     
