d7c2c4d449f9b821a9b27612132a99e48dd49d3e
zzzbray <zachdbray@gmail.com>
2019-05-13 20:30:53 -0500

    made dummy change to force commit new jawsurl

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,7 +11,7 @@ class LoginPage extends Component {
         <form action="/api/signup" method="POST">
           <p>Username</p>
          <input type="text" name="email" placeholder="Enter Username" />
-          <p>Password</p>
+          <p>Your Password</p>
           <input type="password" name="password" placeholder="Enter Password" />
           <input type="submit" name="" value="Sign In" />
           <a href="index.html">Pick Your Pleasure</a>
