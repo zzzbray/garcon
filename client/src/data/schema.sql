3bc3e50e82b88c23ad132f50de5e0476f820c219
zzzbray <zachdbray@gmail.com>
2019-05-15 20:11:24 -0500

    removed user from schema

--- client/src/data/schema.sql
+++ client/src/data/schema.sql
@@ -1,12 +1,3 @@
 DROP DATABASE IF EXISTS garcon_db;
 DROP DATABASE IF EXISTS garcon_sequelize_db;
 CREATE DATABASE garcon_sequelize_db;
-
-CREATE TABLE Users (
- id INTEGER NOT NULL auto_increment,
- email VARCHAR(255) NOT NULL UNIQUE,
- password VARCHAR(255) NOT NULL,
- createdAt DATETIME NOT NULL,
- updatedAt DATETIME NOT NULL,
- PRIMARY KEY (id)
-);
