1fd35d32f106ce3e36039234ef1873f0292a7545
shehanpeiris <shehanpeiris@gmail.com>
2019-05-16 16:17:15 -0500

    removed current receipt component from Waitstaff2OrderPage.js

--- client/src/pages/Waitstaff2OrderPage.js
+++ client/src/pages/Waitstaff2OrderPage.js
@@ -12,7 +12,7 @@ function Waitstaff2OrderPage(props){
     <div class="container">
       <div class="row">
         <div class="col-md-12" >
-        <OrderPageCurReceiptComp />
+        {/* <OrderPageCurReceiptComp /> */}
         <OrderPageMenuComp receiptID={props.match.params.receiptNum} />
         </div>
       </div>
